e4654e69fa10c30c11220dc8fe4d0d279d42e5f9
BlackHat86 <suryansh.c05@gmail.com>
2024-10-06 12:41:13 +0530

    todays commit

--- controller/installment.js
+++ controller/installment.js
@@ -3,20 +3,15 @@ const MonthlyBorrower = require('../models/monthlyborrower');
 
 exports.addDailyInstallment = async (req, res) => {
     try {
-        const { borrowerId } = req.body;
-
+        const { borrowerId, installment } = req.body;
+        console.log(borrowerId, installment)
         // Search for the borrower in the DailyInstallment model
         const borrower = await DailyBorrower.findById(borrowerId);
-
+        console.log(borrower)
         if (!borrower) {
             return res.status(404).json({ message: 'Borrower not found' });
         }
 
-        // If the borrower is found, you can proceed with adding the installment
-        // Add your logic here for adding the installment
-
-        // For now, we'll just return a success message
-        console.log(borrower)
         res.status(200).json({ message: 'Borrower found', borrower });
     } catch (error) {
         res.status(500).json({ message: 'Error processing request', error: error.message });
