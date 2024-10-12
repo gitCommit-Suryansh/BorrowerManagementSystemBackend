2e6deb8f38e9e39a430896ad4315e78a28c3e8f5
BlackHat86 <suryansh.c05@gmail.com>
2024-10-12 14:08:05 +0530

    finance section commit

--- controller/installment.js
+++ controller/installment.js
@@ -209,3 +209,44 @@ exports.fetchFinanceInstallment = async (req, res) => {
       .json({ message: "Error processing request", error: error.message });
   }
 };
+
+exports.addFinanceInstallment = async (req, res) => {
+  try {
+    const { borrowerId, installment } = req.body;
+    // Search for the borrower in the DailyInstallment model
+    const borrower = await FinanceBorrower.findById(borrowerId);
+
+    if (!borrower) {
+      return res.status(404).json({ message: "Borrower not found" });
+    }
+
+    try {
+      const updatedBorrower = await FinanceBorrower.findByIdAndUpdate(
+        borrowerId,
+        {
+          $push: { installments: installment },
+          // $inc: {
+          //   refundedAmount: installment.receivedAmount,
+          //   balanceAmount: -installment.receivedAmount,
+          // },
+          // $set: {
+          //   loanStatus:
+          //     borrower.balanceAmount - installment.receivedAmount <= 0
+          //       ? "closed"
+          //       : borrower.loanStatus,
+          // },
+        },
+        { new: true }
+      );
+      res.status(200).json({message: "Installment added successfully",borrower: updatedBorrower,});
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Error adding installment", error: error.message });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error processing request", error: error.message });
+  }
+};
--- routes/installments.js
+++ routes/installments.js
@@ -5,6 +5,7 @@ const installmentController = require('../controller/installment');
 router
     .post('/adddailyinstallment',installmentController.addDailyInstallment)
     .post('/addmonthlyinstallment',installmentController.addMonthlyInstallment)
+    .post('/addfinanceinstallment',installmentController.addFinanceInstallment)
     .get('/fetchdailyinstallment',installmentController.fetchDailyInstallment)
     .get('/fetchmonthlyinstallment',installmentController.fetchMonthlyInstallment)
     .get('/fetchfinanceinstallment',installmentController.fetchFinanceInstallment)
